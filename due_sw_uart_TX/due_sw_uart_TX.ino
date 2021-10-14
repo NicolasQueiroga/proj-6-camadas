219eb94b15449c3b0dfcaa8c3561444139cfbd7b
NicolasQueiroga <62630822+NicolasQueiroga@users.noreply.github.com>
2021-10-14 19:11:23 -0300

    adicionando TX feito e RX refatorado

---
+++ due_sw_uart_TX/due_sw_uart_TX.ino
@@ -0,0 +1,20 @@
+#include "sw_uart.h"
+
+due_sw_uart uart;
+
+void setup()
+{
+  Serial.begin(9600);
+  sw_uart_setup(&uart, 4, 3, 1, 8, SW_UART_EVEN_PARITY);
+}
+
+void loop()
+{
+  write_letter();
+  delay(600);
+}
+
+void write_letter()
+{
+  sw_uart_write_string(&uart, "a");
+}
---
+++ due_sw_uart_TX/sw_uart.cpp
@@ -0,0 +1,78 @@
+#include "sw_uart.h"
+#pragma GCC optimize("-O3")
+
+void sw_uart_setup(due_sw_uart *uart, int rx, int tx, int stopbits, int databits, int paritybit)
+{
+	uart->pin_tx = tx;
+	uart->stopbits = stopbits;
+	uart->paritybit = paritybit;
+	uart->databits = databits;
+	pinMode(tx, OUTPUT);
+	digitalWrite(tx, HIGH);
+}
+
+int calc_even_parity(char data)
+{
+	int ones = 0;
+	for (int i = 0; i < 8; i++)
+		ones += (data >> i) & 0x01;
+
+	return ones % 2;
+}
+
+void _sw_uart_wait_half_T(due_sw_uart *uart)
+{
+	for (int i = 0; i < 1093; i++)
+		asm("NOP");
+}
+
+void _sw_uart_wait_T(due_sw_uart *uart)
+{
+	_sw_uart_wait_half_T(uart);
+	_sw_uart_wait_half_T(uart);
+}
+
+void sw_uart_write_data(due_sw_uart *uart, char *bufferData, int charLen)
+{
+	for (int i = 0; i < charLen; i++)
+		sw_uart_write_byte(uart, bufferData[i]);
+}
+
+void sw_uart_write_string(due_sw_uart *uart, char *stringData)
+{
+	sw_uart_write_data(uart, stringData, strlen(stringData));
+}
+
+void sw_uart_write_byte(due_sw_uart *uart, char data)
+{
+	int parity = 0;
+	if (uart->paritybit == SW_UART_EVEN_PARITY)
+		parity = calc_even_parity(data);
+	else if (uart->paritybit == SW_UART_ODD_PARITY)
+		parity = !calc_even_parity(data);
+
+	// Sending start bit
+	digitalWrite(uart->pin_tx, LOW);
+	_sw_uart_wait_T(uart);
+
+	// Sending data
+	for (int i = 0; i < uart->databits; i++)
+	{
+		int bit = data >> i & 1;
+		digitalWrite(uart->pin_tx, bit);
+		_sw_uart_wait_T(uart);
+	}
+
+	if (uart->paritybit != SW_UART_NO_PARITY)
+	{
+		digitalWrite(uart->pin_tx, parity);
+		_sw_uart_wait_T(uart);
+	}
+
+	// Sending stop bit
+	for (int i = 0; i < uart->stopbits; i++)
+	{
+		digitalWrite(uart->pin_tx, HIGH);
+		_sw_uart_wait_T(uart);
+	}
+}
